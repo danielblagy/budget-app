a6656893d7e427e00a70b7d4b8057c28e020ff25
Daniel Blagy <daniel.blagy@gmail.com>
2023-06-24 15:45:21 +0300

    #43 missing semicolon in sql to test squawk sql error

--- migrations/000001_create_users.up.sql
+++ migrations/000001_create_users.up.sql
@@ -4,7 +4,7 @@ create table users (
     email           text            not null unique,
     full_name       text            not null,
     password_hash   text            not null
-);
+)
 
 insert into users(username, email, full_name, password_hash)
 values ('testusername111', 'testemail111@mail.com', 'John Doe', '$2a$10$eB3Axm6ikuREMCwYlxgrgOuEqjxL7r20ZIgaWziIL8JajzuXRQ6HW');
